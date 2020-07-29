7c2f8fc98cf987e1f7651ed0b7044a25d329018e
CallOfCrutches <foward080695@gmail.com>
2020-07-29 18:39:43 +0300

    Added tuple_reduce tests

--- tests/omp/utils/common_tests.cpp
+++ tests/omp/utils/common_tests.cpp
@@ -137,16 +137,6 @@ TEST( omp_tuple_map_tuple, one_tuple )
   ASSERT_EQ( expected, result );
 }
 
-TEST( omp_tuple_map_value, simple_use_case )
-{
-  auto initial = std::make_tuple( 1, 2.0, 'a', 4, 5 );
-  auto expected = std::make_tuple( 2, 3.0, 'b', 5, 6 );
-
-  auto result = omp::tuple_map( []( const auto& val ) { return val + 1; }, initial );
-
-  ASSERT_EQ( expected, result );
-}
-
 TEST( omp_tuple_map_tuple, one_tuple_ref )
 {
   auto expected = std::make_tuple( 1, 2.0, 'a', 4, 5 );
@@ -161,7 +151,7 @@ TEST( omp_tuple_map_tuple, one_tuple_ref )
   ASSERT_EQ( expected, result );
 }
 
-TEST( omp_tuple_map_tuple, multi_tuple )
+TEST( omp_tuple_map_tuple, multiple_tuple )
 {
   auto expected1 = std::make_tuple( 1, 2.0, 'a', 4, 5 );
   auto expected2 = std::make_tuple( 5, 4.0, 'z', 2, 1 );
@@ -175,7 +165,7 @@ TEST( omp_tuple_map_tuple, multi_tuple )
   ASSERT_EQ( std::get<4>( result ), std::get<4>( expected1 ) + std::get<4>( expected2 ) );
 }
 
-TEST( omp_tuple_map_tuple, multi_tuple_same )
+TEST( omp_tuple_map_tuple, multiple_tuple_same )
 {
   auto expected1 = std::make_tuple( 1, 2.0, 'a', 4, 5 );
 
@@ -261,7 +251,7 @@ TEST( omp_tuple_map_pair, one_pair_ref )
     ASSERT_EQ( 0, expected.second );
 }
 
-TEST( omp_tuple_map_pair, multi_pair )
+TEST( omp_tuple_map_pair, multiple_pair )
 {
     auto expected1 = std::pair( 1, 2.0 );
     auto expected2 = std::pair( 3, 5.0 );
@@ -298,7 +288,7 @@ TEST( omp_tuple_map_array, one_array_ref )
     ASSERT_EQ( &std::get<4>( result ), &expected[4] );
 }
 
-TEST( omp_tuple_map_array, multi_arrays )
+TEST( omp_tuple_map_array, multiple_arrays )
 {
     auto expected1 = std::array{ 1, 2, 3, 4, 5 };
     auto expected2 = std::array{ 5, 4, 3, 2, 1 };
@@ -326,4 +316,76 @@ TEST( omp_tuple_map_different_types, tuple_pair_array )
 
   ASSERT_EQ( std::get<0>( result ), 3 );
   ASSERT_EQ( std::get<1>( result ), 6.f );
+}
+
+TEST( omp_tuple_reduce_tuple, one_tuple )
+{
+  auto expected = std::make_tuple( 1, 2.0, 'a', 4, 5 );
+  auto result = omp::tuple_reduce( []( auto accum, auto& val ) { return accum + val; }, 0, expected );
+
+  ASSERT_EQ( result, 1 + 2.0 + 'a' + 4 + 5 );
+}
+
+TEST( omp_tuple_reduce_tuple, multiple_tuples )
+{
+  auto expected1 = std::make_tuple( 1, 2.0, 'a', 4, 5 );
+  auto expected2 = std::make_tuple( 5, 4.0, 'z', 2, 1 );
+
+  auto result = omp::tuple_reduce( []( auto accum, auto& val1, auto& val2 ) { return accum + val1 + val2; }, 0,
+                                   expected1, std::as_const( expected2 ) );
+
+  ASSERT_EQ( result, 1 + 2.0 + 'a' + 4 + 5 + 5 + 4.0 + 'z' + 2 + 1 );
+}
+
+TEST( omp_tuple_reduce_pair, one_pair )
+{
+  auto expected = std::pair( 2, 3 );
+
+  auto result = omp::tuple_reduce( []( auto accum, auto& val ) { return accum * val; }, 1, expected );
+
+  ASSERT_EQ( result, 6 );
+}
+
+TEST( omp_tuple_reduce_pair, multiple_pairs )
+{
+  auto expected1 = std::pair( 2, 3 );
+  auto expected2 = std::pair( 5, 7 );
+
+  auto result = omp::tuple_reduce( []( auto accum, auto& val1, auto& val2 ) { return accum * val1 * val2; }, 1,
+                                   std::as_const( expected1 ), expected2 );
+
+  ASSERT_EQ( result, 210 );
+}
+
+TEST(omp_tuple_reduce_array, one_array)
+{
+  auto expected = std::array{ 1, 2, 3, 4, 5 };
+
+  auto result = omp::tuple_reduce( []( auto accum, auto& val ) { return accum * val; }, 1, expected );
+
+  ASSERT_EQ( result, 120 );
+}
+
+TEST( omp_tuple_reduce_array, multiple_arrays )
+{
+  auto expected1 = std::array{ 1, 2, 3, 4, 5 };
+  auto expected2 = std::array{ 5, 4, 3, 2, 1 };
+
+  auto result = omp::tuple_reduce( []( auto accum, auto& val1, auto& val2 ) { return accum * val1 * val2; }, 1,
+                                   std::as_const( expected1 ), std::as_const( expected2 ) );
+
+  ASSERT_EQ( result, 120 * 120 );
+}
+
+TEST( omp_tuple_reduce_different_types, tuple_array_pair )
+{
+  auto expected1 = std::array{ 1, 2, };
+  auto expected2 = std::pair { 3, 4, };
+  auto expected3 = std::tuple{ 5, 6, };
+
+  auto result = omp::tuple_reduce( []( auto accum, auto& val1, auto& val2, auto& val3 )
+                                   { return accum * val1 * val2 * val3; }, 1,
+                                   std::as_const( expected1 ), std::as_const( expected2 ), expected3 );
+
+  ASSERT_EQ( result, 720  );
 }

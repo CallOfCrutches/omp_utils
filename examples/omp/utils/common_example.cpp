dc08cdf4e1a84ea7804af5e80c6178f8ddc8ec23
CallOfCrutches <foward080695@gmail.com>
2020-09-15 10:18:21 +0300

    Remove omp::min, omp::max, omp::sum

--- examples/omp/utils/common_example.cpp
+++ examples/omp/utils/common_example.cpp
@@ -1,43 +1,11 @@
 #include "omp/utils/common.h"
 
-
 #include <iostream>
 #include <set>
 
 
-namespace
-{
-  enum class Channels
-  {
-    Alpha = 0,
-    Red = 1,
-    Green = 2,
-    Blue = 3,
-  };
-
-  void min_example()
-  {
-    std::cout << "min of elements: 1, 5, 2, 0, -5.4f, 2.1, -5.5 is " << omp::min( 1, 5, 2, 0, -5.4f, 2.1, -5.5 ) << "\n";
-  }
-
-  void max_example()
-  {
-    std::cout << "max of elements: 1, 5, 2, 0, -5.4f, 2.1, -5.5 is " << omp::max( 1, 5, 2, 0, -5.4f, 2.1, -5.5 ) << "\n";
-  }
-
-  void sum_example()
-  {
-    std::cout << "sum of elements: 1, 5, 2, 0, -5.4f, 2.1, -5.5 is " << omp::sum( 1, 5, 2, 0, -5.4f, 2.1, -5.5 ) << "\n";
-  }
-}
-
 void common_examples()
 {
   std::cout << "common examples\n";
-
-  min_example();
-  max_example();
-  sum_example();
-
   std::cout << std::endl;
 }
--- include/omp/utils/common.h
+++ include/omp/utils/common.h
@@ -9,44 +9,6 @@ namespace omp
 
   namespace details
   {
-    template<typename T>
-    struct collector_value
-    {
-      explicit collector_value( const T& value ) noexcept
-        : value( value )
-      {}
-
-      const T& value;
-    };
-
-    template<typename T, typename F>
-    struct collector
-    {
-      collector( T value, F func ) noexcept
-        : value( std::move( value ) )
-        , func( std::move( func ) )
-      {}
-
-      template<typename V>
-      auto operator|( const collector_value<V>& cvalue )
-      {
-        using common_t = std::common_type_t<T, V>;
-        // TODO: make something with types
-        using collector_type = decltype( func( static_cast<common_t>( value ),
-                                               static_cast<common_t>( cvalue.value ) ) );
-
-        return collector<collector_type, F>( func( static_cast<common_t>( value ),
-                                                   static_cast<common_t>( cvalue.value ) ),
-                                             std::move( func ) );
-      }
-
-      T value;
-      F func;
-    };
-
-    template<typename T, typename F>
-    collector( const T&, F&& ) -> collector<T, F>;
-
     template<typename Tuple>
     constexpr bool support_get_one_( decltype( std::get<0>( std::declval<Tuple>() ), int() ) ) { return true; }
     template<typename>
@@ -104,39 +66,6 @@ namespace omp
 
       return initial;
     }
-  }
-
-  template<typename First, typename... Others>
-  auto min( const First& first, const Others&... others )
-  {
-    using details::collector;
-    using details::collector_value;
-
-    auto min_generic = []( const auto& f, const auto& s ) { return f < s ? f : s; };
-
-    return ( collector( first, std::move( min_generic ) ) | ... | collector_value<Others>( others ) ).value;
-  }
-  
-  template<typename First, typename... Others>
-  auto max( const First& first, const Others&... others )
-  {
-    using details::collector;
-    using details::collector_value;
-
-    auto max_generic = []( const auto& f, const auto& s ) { return f > s ? f : s; };
-
-    return ( collector( first, std::move( max_generic ) ) | ... | collector_value<Others>( others ) ).value;
-  }
-
-  template<typename First, typename... Others>
-  auto sum( const First& first, const Others&... others )
-  {
-    using details::collector;
-    using details::collector_value;
-
-    auto plus_generic = []( const auto& f, const auto& s ) { return f + s; };
-
-    return ( collector( first, std::move( plus_generic ) ) | ... | collector_value<Others>( others ) ).value;
   }
 
   template<typename Callable, typename... Tuples,

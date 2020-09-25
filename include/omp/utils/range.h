a61a437ca0e40be9ee3a23d94ad26064604956e6
CallOfCrutches <foward080695@gmail.com>
2020-09-25 10:21:36 +0300

    Remake omp::range

--- include/omp/utils/range.h
+++ include/omp/utils/range.h
@@ -23,24 +23,23 @@ namespace omp
       using difference_type = value_type;
 
       range_iterator( value_type value, value_type step )
-        : step( step )
-        , value( value )
+        : value_( value )
+        , step_( step )
       {}
 
       reference operator*() const noexcept
       {
-        return value;
+        return value_;
       }
 
       pointer operator->() const noexcept
       {
-        return &value;
+        return &value_;
       }
 
       range_iterator& operator++() noexcept
       {
-        value += step;
-
+        value_ += step_;
         return *this;
       }
 
@@ -52,116 +51,88 @@ namespace omp
         return temp;
       }
 
-      bool operator==( const range_iterator& second ) const noexcept
+      bool operator==( const range_iterator& rhs ) const noexcept
       {
-        return value == second.value;
+        return step_ != rhs.step_ || compare( value_, rhs.value_, step_ );
       }
 
-      bool operator!=( const range_iterator& second ) const noexcept
+      bool operator!=( const range_iterator& rhs ) const noexcept
       {
-        return !( *this == second );
-      }
-
-      bool operator<( const range_iterator& second ) const noexcept
-      {
-        return value < second.value;
-      }
-
-      bool operator>( const range_iterator& second ) const noexcept
-      {
-        return second < *this;
-      }
-
-      bool operator<=( const range_iterator& second ) const noexcept
-      {
-        return !( second < *this );
-      }
-
-      bool operator>=( const range_iterator& second ) const noexcept
-      {
-        return !( *this < second );
+        return !( *this == rhs );
       }
 
     private:
-      const value_type step;
-      value_type value;
-    };
-
-
-    template<typename Integral>
-    struct range
-    {
-      using value_type = Integral;
-
-      using iterator       = range_iterator<value_type>;
-      using const_iterator = range_iterator<value_type>;
-
-      range( value_type start, value_type stop, value_type step ) noexcept
-        : start_( start )
-        , stop_( stop )
-        , step_( step )
-      {}
-
-      const_iterator begin() const noexcept
+      bool compare( value_type lhs, value_type rhs, value_type sign ) const noexcept
       {
-        return const_iterator( start_, step_ );
-      }
-
-      const_iterator end() const noexcept
-      {
-        return const_iterator( stop_, step_ );
-      }
-
-      bool empty() const noexcept
-      {
-        return start_ == stop_;
-      }
-
-      std::size_t size() const noexcept
-      {
-        return static_cast<std::size_t>( ( stop_ - start_ ) / step_ );
+          return sign > 0 ? lhs >= rhs : lhs <= rhs;
       }
 
     private:
-      const value_type start_;
-      const value_type stop_;
       const value_type step_;
+      value_type value_;
     };
-
-    template<typename Integral>
-    inline Integral align_stop( Integral start, Integral stop, Integral step ) noexcept
-    {
-      if( start == stop )
-        return start;
-
-      if( start > stop && step > 0 )
-        return start;
-
-      if( start < stop && step < 0 )
-        return start;
-
-      auto mod = ( stop - start ) % step;
-
-      return mod != 0 ? stop + step - mod : stop;
-    }
-  }
-
-  template<typename Start, typename Stop, typename Step = std::common_type_t<Start, Stop>>
-  inline auto range( Start start, Stop stop, Step step = Step{1} )
-  {
-    using Integral = std::common_type_t<Start, Stop, Step>;
-
-    static_assert( std::is_integral_v<Integral>, "Type is not integral" );
-
-    if( !step ) throw std::logic_error( "step is zero" );
-
-    stop = details::align_stop<Integral>( start, stop, step );
-    return details::range<Integral>( start, stop, step );
   }
 
   template<typename Integral>
-  inline auto range( Integral stop )
+  struct range
   {
-    return range( Integral{0}, stop );
-  }
+    using value_type = Integral;
+
+    using iterator       = details::range_iterator<value_type>;
+    using const_iterator = details::range_iterator<value_type>;
+
+    static_assert( std::is_integral_v<value_type>, "Type is not integral" );
+
+    range( value_type stop )
+      : range( value_type{ 0 }, stop, value_type{ 1 } )
+    {}
+
+    range( value_type start, value_type stop )
+      : range( start, stop, value_type{ 1 } )
+    {}
+
+    range( value_type start, value_type stop, value_type step )
+      : start_( start )
+      , stop_( stop )
+      , step_( step )
+    {
+      if( !step_ )
+        throw std::logic_error("step is zero");
+    }
+
+    const_iterator begin() const noexcept
+    {
+      return const_iterator( start_, step_ );
+    }
+
+    const_iterator end() const noexcept
+    {
+      return const_iterator( stop_, step_ );
+    }
+
+    bool empty() const noexcept
+    {
+      return start_ == stop_;
+    }
+
+    std::size_t size() const noexcept
+    {
+      return static_cast<std::size_t>( ( stop_ - start_ ) / step_ );
+    }
+
+  private:
+    const value_type start_;
+    const value_type stop_;
+    const value_type step_;
+  };
+
+  template<typename Int>
+  range( Int ) -> range<Int>;
+
+  template<typename Int1, typename Int2>
+  range( Int1, Int2 ) -> range<std::common_type_t<Int1, Int2>>;
+
+  template<typename Int1, typename Int2, typename Int3>
+  range( Int1, Int2, Int3 ) -> range<std::common_type_t<Int1, Int2, Int3>>;
+
 }
